c26302859885a71cf945303e75c784fe80ebd007
Abhishek Gupta <abhishekmgupta@google.com>
2026-06-16 10:43:35 +0000

    Refactor: use errors.Is for ENODEV/EINTR error handling

--- connection.go
+++ connection.go
@@ -16,6 +16,7 @@ package fuse
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -398,17 +399,12 @@ func (c *Connection) readMessage() (*buffer.InMessage, error) {
 		//  *  EINTR means we should try again. (This seems to happen often on
 		//     OS X, cf. http://golang.org/issue/11180)
 		//
-		if pe, ok := err.(*os.PathError); ok {
-			switch pe.Err {
-			case syscall.ENODEV:
-				err = io.EOF
-
-			case syscall.EINTR:
-				err = nil
-				continue
-			}
+		if errors.Is(err, syscall.ENODEV) {
+			err = io.EOF
+		} else if errors.Is(err, syscall.EINTR) {
+			err = nil
+			continue
 		}
-
 		if err != nil {
 			c.putInMessage(m)
 			return nil, err
